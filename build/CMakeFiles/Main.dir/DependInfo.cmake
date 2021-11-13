deb1c43365145ff10febd405ebf06232d5bfffa0
Jiaqi Ma <majiaqi808@gmail.com>
2021-11-13 06:33:45 -0800

    original file

---
+++ build/CMakeFiles/Main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jiiaqi/OptiBProg1/Main.cpp" "/home/jiiaqi/OptiBProg1/build/CMakeFiles/Main.dir/Main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jiiaqi/OptiBProg1/build/CMakeFiles/readLgf.dir/DependInfo.cmake"
+  "/home/jiiaqi/OptiBProg1/build/CMakeFiles/check.dir/DependInfo.cmake"
+  "/home/jiiaqi/OptiBProg1/build/CMakeFiles/random.dir/DependInfo.cmake"
+  "/home/jiiaqi/OptiBProg1/build/CMakeFiles/spanningTree.dir/DependInfo.cmake"
+  "/home/jiiaqi/OptiBProg1/build/CMakeFiles/shortestPath.dir/DependInfo.cmake"
+  "/home/jiiaqi/OptiBProg1/build/CMakeFiles/steiner.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/shortestPath.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shortestPath.dir/myShortestPath.cpp.o"
+  "libshortestPath.a"
+  "libshortestPath.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shortestPath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
