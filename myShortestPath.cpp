94a93e907d2a15dbeb61aa7c6348821811bb52cc
Siyu Chen <snape1996@163.com>
2021-11-16 15:31:48 +0100

    shortest path with Dijkstra

--- myShortestPath.cpp
+++ myShortestPath.cpp
@@ -1,29 +1,28 @@
 #include "myShortestPath.h"
-typedef std::pair<Vertex,int> V_and_distance;
-struct CmpByValueGreat{
-  bool operator()(const V_and_distance &lhs,const V_and_distance &rhs){
-    return lhs.second < rhs.second ;
-  }
-};
 
+typedef std::pair<Vertex,int> V_and_distance;
 
 std::vector<Vertex> my_shortest_path(const Graph &g, const Vertex &startVertex,
                                      const Vertex &endVertex) {
   std::vector<Vertex> path;
   // compute a shortest path between startVertex and endVertex and save it in
   // the vector >>path<<
+
+  struct {
+    bool operator()(std::pair<Vertex, int> p1, std::pair<Vertex, int> p2) const{
+        return p1.second < p2.second;
+    }
+  }sort_pred;
   
   //Dijstra
   int num_vertices = boost::num_vertices(g);
-
-  std::priority_queue<V_and_distance,std::vector<V_and_distance>,CmpByValueGreat> Dijstra;
-
-  std::vector<Vertex> pred_vertex(num_vertices);
-
-  
+  std::vector<V_and_distance> Dijstra;
+  std::vector<int> pred_vertex(num_vertices);
   int distance[num_vertices];
+
   for(int i = 0;i<num_vertices;i++){
     distance[i] = std::numeric_limits<int>::max();
+    pred_vertex[i] = -1;
   }
 
   std::vector<Vertex> V_all;
@@ -35,46 +34,30 @@ std::vector<Vertex> my_shortest_path(const Graph &g, const Vertex &startVertex,
   //Inizialisirung
   distance[(int)startVertex] = 0;
 
-
-  Dijstra.push(std::make_pair(startVertex,0));
+  Dijstra.push_back(std::make_pair(startVertex,0));
   pred_vertex[(int)startVertex] = startVertex;
 
-  std::cout<<"hier!!!!!!!!!!!!!"<<std::endl;
   while (!Dijstra.empty()){
-    auto curr = Dijstra.top();
-    Dijstra.pop();
+    auto curr = Dijstra[0];
+    Dijstra.erase(Dijstra.begin());
     if(curr.first == endVertex){break;}
-    auto out_edges = boost::out_edges(Dijstra.top().first,g);
+    auto out_edges = boost::out_edges(curr.first,g);
     for(auto adj_edges : make_iterator_range(out_edges)){
-      if(curr.second
-      + (int) boost::get(boost::edge_weight,g,adj_edges)
-      < distance[boost::target(adj_edges,g)]){
-        
-        distance[boost::target(adj_edges,g)] = curr.second
-        + (int) boost::get(boost::edge_weight,g,adj_edges);
-        pred_vertex[boost::target(adj_edges,g)] = Dijstra.top().first;
-        Dijstra.push(std::make_pair(boost::target(adj_edges,g),distance[boost::target(adj_edges,g)]));
+      if(curr.second + (int) boost::get(boost::edge_weight,g,adj_edges) < distance[boost::target(adj_edges,g)]){
+        distance[boost::target(adj_edges,g)] = curr.second + (int) boost::get(boost::edge_weight,g,adj_edges);
+        pred_vertex[boost::target(adj_edges,g)] = (int)curr.first;
+        Dijstra.push_back(std::make_pair(boost::target(adj_edges,g),distance[boost::target(adj_edges,g)]));
       }    
     }
-    //not_scanned_V.erase(not_scanned_V.begin()+(int)Dijstra.top().first);
+    std::sort(Dijstra.begin(),Dijstra.end(),sort_pred);
+    
   }
-  
-  std::cout<<"[";
-  for(int i=0;i<(int)sizeof(pred_vertex);i++){
-    std::cout<<pred_vertex[i]<<",";
-  }
-  std::cout<<"]"<<std::endl;
 
-
-  // std::vector<Vertex> all_V;
-  // VertexIterator v,v_end;
-  // for(std::tie(vi,vi_end) = boost::vertices(g); vi != vi_end; ++vi){
-  //     all_V.push_back(*vi);
-  // }
   int look_for = (int)endVertex;
+  path.push_back(V_all[endVertex]);
   while (look_for != (int)startVertex)
   {
-    path.push_back(V_all[pred_vertex[look_for]]);
+    path.insert(path.begin(), V_all[pred_vertex[look_for]]);
     look_for = pred_vertex[look_for];
   }
 
